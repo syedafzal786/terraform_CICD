051b34309c2888bcacf65a29ee7e356a2043f2c6
Syed Afzal <168406355+syedafzal786@users.noreply.github.com>
2024-05-27 18:15:45 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "ap-south-2"
+}
+
+resource "aws_instance" "ayush" {
+  ami           = "ami-0cc9838aa7ab1dce7"
+  instance_type = "t2.micro"
+  key_name      = "syed2"
+
+  tags = {
+    Name = "ayush"
+  }
+}
