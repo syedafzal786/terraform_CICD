e5591df34cf05e95e1446d91ca73570e558f5133
Syed Afzal <168406355+syedafzal786@users.noreply.github.com>
2024-05-27 19:02:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "ap-southeast-1"
 }
 
-resource "aws_instance" "ayush" {
+resource "aws_instance" "abdul" {
   ami           = "ami-0ac9397cab55f5044"
   instance_type = "t2.micro"
   key_name      = "syed2"
